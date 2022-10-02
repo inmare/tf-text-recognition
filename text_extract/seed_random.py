7b4eb68d3e0b483074711a8871b7d89e4d43a052
yatsumehole <yatsumehole@naver.com>
2022-10-02 10:22:12 +0000

    파일 오류 및 변수 명 수정

--- text_extract/seed_random.py
+++ text_extract/seed_random.py
@@ -1,9 +1,12 @@
+import numpy as np
+
+
 class random:
     def __init__(self, seed):
         self.seed = seed
 
-    def rand(self):
-        self.seed = (1013904223 + 1664525 * self.seed) % 4294976296
+    def random(self):
+        self.seed = (1013904223 + 1664525 * self.seed) % 4294967296
         return self.seed / 4294967296
 
     def choice(self, array):
@@ -21,14 +24,14 @@ def make_random_text(seed: int, text_len: int) -> np.ndarray:
     Returns:
         np.ndarray: 랜덤한 텍스트의 아스키코드들을 ndarray형태로 반환함
     """
-    random = seed_random(seed)
-    char_list = [i for i in range(0x21, 0x7F)]
+    rnd = random(seed)
+    char_list = [i - 0x21 for i in range(0x21, 0x7F)]
 
-    random.random()  # 더 랜덤한 값을 위해 랜덤 함수를 한 번 실행해 줌
+    rnd.random()  # 더 랜덤한 값을 위해 랜덤 함수를 한 번 실행해 줌
     random_text = []
 
     for i in range(text_len):
-        random_text.append(random.choice(char_list))
+        random_text.append(rnd.choice(char_list))
 
     return np.asarray(random_text, dtype=np.int8)
 
