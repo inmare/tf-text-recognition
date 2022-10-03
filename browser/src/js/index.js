114c23ccaa47387742ba26e1f913192f47c4e061
yatsumehole <yatsumehole@naver.com>
2022-10-03 11:18:23 +0000

    분리된 클래스에 맟게 코드 수정

--- browser/src/js/index.js
+++ browser/src/js/index.js
@@ -1,1 +1,38 @@
+/* eslint-disable no-unused-vars */
+// node_modules
+import cv from "@techstark/opencv-js";
+// custom modules
+import Process from "./process.js";
+import Setting from "./setting.js";
+// css
 import "../css/style.css";
+// images
+import textImg from "../image/sample-text.jpg";
+
+const original = document.querySelector("#original-canvas");
+const current = document.querySelector("#current-canvas");
+
+cv.onRuntimeInitialized = main;
+
+function main() {
+  const imageElem = new Image();
+  imageElem.src = textImg;
+  imageElem.onload = processImage;
+}
+
+function processImage(e) {
+  const imageElem = e.target;
+  const image = cv.imread(imageElem);
+  const grayImg = Process.gray(image);
+  image.delete();
+  const cropRect = Setting.crop;
+  const cropImg = Process.crop(grayImg, cropRect);
+  grayImg.delete();
+
+  const binaryThresh = Setting.binaryThresh;
+  const binaryImg = Process.binarization(cropImg, binaryThresh, true);
+  cv.imshow(original, cropImg);
+  cropImg.delete();
+  cv.imshow(current, binaryImg);
+  binaryImg.delete();
+}
