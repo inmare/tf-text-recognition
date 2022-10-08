21b0c0a03d65f34015ee2e0d29ac5b3118fe7560
yatsumehole <yatsumehole@naver.com>
2022-10-08 08:55:34 +0000

    getCharPoint함수 추가

--- browser/src/js/index.js
+++ browser/src/js/index.js
@@ -3,6 +3,7 @@
 import cv from "@techstark/opencv-js";
 // custom modules
 import Extract from "./extract.js";
+import Data from "./data.js";
 // css
 import "../css/style.css";
 // images
@@ -23,6 +24,9 @@ function processImage(e) {
   const rotateImg = Extract.setImageRotation(e.target);
   const textbox = Extract.getTextbox(rotateImg);
   const clearTextbox = Extract.enhanceTextbox(textbox);
+  textbox.delete();
+
+  Data.getCharPoint(clearTextbox);
 
   cv.imshow(original, rotateImg);
   cv.imshow(current, clearTextbox);
