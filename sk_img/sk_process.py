0340da8f2ec610471861b67618ca802bc6d4b8a5
yatsumehole <yatsumehole@naver.com>
2022-10-15 15:32:47 +0000

    opencv관련 함수들을 scikit-image 함수들로 변경

--- sk_img/sk_process.py
+++ sk_img/sk_process.py
@@ -1,5 +1,9 @@
-from skimage import io, color, filters, morphology, transform
+from skimage import io, color, filters, morphology, transform, restoration, exposure
 import numpy as np
+
+
+def gray(image):
+    return color.rgb2gray(image)
 
 
 def read(file_path: str, do_gray: bool = True) -> np.ndarray:
@@ -7,10 +11,14 @@ def read(file_path: str, do_gray: bool = True) -> np.ndarray:
     crop_image = image[400:700, :400]
 
     if do_gray and len(crop_image.shape) > 2:
-        gray_image = color.rgb2gray(crop_image)
+        gray_image = gray(crop_image)
         return gray_image
     else:
         return crop_image
+
+
+def save(image: np.ndarray, save_path: str):
+    io.imsave(f"{save_path}/sample-textbox.png", image)
 
 
 def invert(image: np.ndarray) -> np.ndarray:
@@ -19,27 +27,26 @@ def invert(image: np.ndarray) -> np.ndarray:
     return invert_img
 
 
-def binarization(image: np.ndarray, thresh: int, do_invert: bool = True) -> np.ndarray:
+def binarize(image: np.ndarray, thresh: int, do_invert: bool = True) -> np.ndarray:
+    binary_img = image > thresh
+
     invert_img = None
-
     if do_invert:
-        invert_img = invert(image)
+        invert_img = invert(binary_img)
     else:
-        invert_img = image
+        invert_img = binary_img
 
-    binary_img = invert_img > thresh
-
-    return binary_img
+    return invert_img
 
 
-def dilation(image: np.ndarray, kernel_size: list) -> np.ndarray:
+def dilate(image: np.ndarray, kernel_size: list) -> np.ndarray:
     kernel = np.ones(tuple(kernel_size))
     dilate_img = morphology.dilation(image, kernel)
 
     return dilate_img
 
 
-def erosion(image: np.ndarray, kernel_size: list) -> np.ndarray:
+def erode(image: np.ndarray, kernel_size: list) -> np.ndarray:
     kernel = np.ones(tuple(kernel_size))
     erode_img = morphology.erosion(image, kernel)
 
@@ -47,24 +54,21 @@ def erosion(image: np.ndarray, kernel_size: list) -> np.ndarray:
 
 
 def denoise(
-    image: np.ndarray, strength: int, templateSize: int, searchSize: int
+    image: np.ndarray, kernel_size: int, sigma_color: float, sigma_space: float
 ) -> np.ndarray:
-    denoise_img = cv2.fastNlMeansDenoising(
-        image, None, strength, templateSize, searchSize
+    denoise_img = restoration.denoise_bilateral(
+        image, kernel_size, sigma_color, sigma_space
     )
 
     return denoise_img
 
 
-def contrast(image: np.ndarray, amount: int()):
-    # 출처: https://stackoverflow.com/questions/39308030/how-do-i-increase-the-contrast-of-an-image-in-python-opencv
-    f = 131 * (amount + 127) / (127 * (131 - amount))
-    alpha_c = f
-    gamma_c = 127 * (1 - f)
+def contrast(image: np.ndarray, contrast_min: float, contrast_max: float) -> np.ndarray:
+    contrast_img = exposure.rescale_intensity(
+        image, in_range=(contrast_min, contrast_max)
+    )
 
-    image = cv2.addWeighted(image, alpha_c, image, 0, gamma_c)
-
-    return image
+    return contrast_img
 
 
 def rotate(image: np.ndarray, angle: float, center: list) -> np.ndarray:
